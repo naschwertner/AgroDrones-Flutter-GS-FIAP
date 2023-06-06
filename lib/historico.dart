b3c190c71d1b1b717c8bbd7235a572a2d2bbfec1
Nathalia Schwertner <nathalia_13@outlook.com>
2023-06-05 22:43:39 -0300

    historico

--- lib/historico.dart
+++ lib/historico.dart
@@ -9,38 +9,44 @@ class Historico extends StatelessWidget {
     {
       'url':
           'https://as1.ftcdn.net/v2/jpg/02/64/83/16/1000_F_264831691_dRX2OwpVvOW24mben6VIj2q5sHhJA5Ke.jpg',
-      'id': 'ID195515435616',
-      'name': 'Drone 1'
+      'id': 'ID706207951021',
+      'name': 'Drone 1',
+      'data': 'Dia 03/04/2023'
     },
     {
       'url':
           'https://as2.ftcdn.net/v2/jpg/02/44/49/75/1000_F_244497526_G8VVBifcserH9NMKLwME0LyOXNOemEpt.jpg',
-      'id': 'ID195515435617',
-      'name': 'Drone 2'
+      'id': 'ID245689456124',
+      'name': 'Drone 2',
+      'data': 'Dia 13/06/2023'
     },
     {
       'url':
           'https://as2.ftcdn.net/v2/jpg/03/33/81/09/500_F_333810946_myQ5U5TlGfOfESvI9FrKEf8ELUcWdNCG.jpg',
-      'id': 'ID195515435617',
-      'name': 'Drone 3'
+      'id': 'ID867943102586',
+      'name': 'Drone 3',
+      'data': 'Dia 06/06/2023'
     },
     {
       'url':
           'https://as1.ftcdn.net/v2/jpg/02/98/24/22/1000_F_298242226_jw0LilQmIALroH1LQScpIk9cjcLsFcbp.jpg',
-      'id': 'ID195515435617',
-      'name': 'Drone 4'
+      'id': 'ID153287046982',
+      'name': 'Drone 4',
+      'data': 'Dia 10/04/2023'
     },
     {
       'url':
           'https://as1.ftcdn.net/v2/jpg/02/42/56/26/500_F_242562684_5VC68EHElXConqPjRazxCGuJk6btgmec.jpg',
-      'id': 'ID195515435617',
-      'name': 'Drone 5'
+      'id': 'ID512906374810',
+      'name': 'Drone 5',
+      'data': 'Dia 09/05/2023'
     },
     {
       'url':
           'https://t4.ftcdn.net/jpg/02/44/49/75/360_F_244497513_pj9qN7NuQHYdAEMtLOEvHAjXZlv6bTbG.webp',
-      'id': 'ID195515435617',
-      'name': 'Drone 6'
+      'id': 'ID389765410293',
+      'name': 'Drone 6',
+      'data': 'Dia 27/06/2023'
     },
   ];
 
@@ -77,6 +83,12 @@ class Historico extends StatelessWidget {
                     SizedBox(height: 5.0),
                     Text(
                       imgData[index]['name']!,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    Text(
+                      imgData[index]['data']!,
                       style: TextStyle(
                         fontSize: 14.0,
                       ),

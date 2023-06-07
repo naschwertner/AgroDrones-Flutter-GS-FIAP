3697b5de9ee93abb340c010b8afd3cebce7a6f84
Nathalia Schwertner <nathalia_13@outlook.com>
2023-06-07 12:41:38 -0300

    corrige historico de imagens

--- lib/historico.dart
+++ lib/historico.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 import 'AnaliseImgHistorico.dart';
@@ -25,35 +27,45 @@ class _HistoricoState extends State<Historico> {
           'https://as2.ftcdn.net/v2/jpg/02/44/49/75/1000_F_244497526_G8VVBifcserH9NMKLwME0LyOXNOemEpt.jpg',
       'id': 'ID195515435623',
       'name': 'Drone 2',
-      'data': 'Dia 01/06/2023'
+      'data': 'Dia 01/06/2023',
+      'analysis':
+          'Através da análise de uma imagem capturada por um drone em uma plantação, foi possível observar que o solo apresenta uma textura adequada e indícios de nutrientes presentes. Não foram encontradas evidências de pragas ou doenças. Sugere-se monitorar a umidade do solo e o crescimento da cobertura vegetal para manter o desenvolvimento saudável das plantas.'
     },
     {
       'url':
           'https://as2.ftcdn.net/v2/jpg/03/33/81/09/500_F_333810946_myQ5U5TlGfOfESvI9FrKEf8ELUcWdNCG.jpg',
       'id': 'ID195515435630',
       'name': 'Drone 3',
-      'data': 'Dia 27/05/2023'
+      'data': 'Dia 27/05/2023',
+      'analysis':
+          'Com base em uma análise de imagem de solo agrícola obtida por um drone, verificou-se que o solo apresenta uma textura favorável e indícios de nutrientes. Não foram identificadas pragas ou doenças aparentes. Recomenda-se monitorar a umidade do solo e acompanhar o crescimento da cobertura vegetal para garantir um bom desenvolvimento da plantação.'
     },
     {
       'url':
           'https://as1.ftcdn.net/v2/jpg/02/98/24/22/1000_F_298242226_jw0LilQmIALroH1LQScpIk9cjcLsFcbp.jpg',
       'id': 'ID195515435647',
       'name': 'Drone 4',
-      'data': 'Dia 22/05/2023'
+      'data': 'Dia 22/05/2023',
+      'analysis':
+          'Utilizando a análise de uma imagem de solo agrícola capturada por um drone, constatou-se que o solo exibe uma textura adequada e indícios de nutrientes presentes. Não foram encontrados sinais visíveis de pragas ou doenças. Recomenda-se monitorar a umidade do solo e a evolução da cobertura vegetal para manter a saúde e o crescimento das plantas.'
     },
     {
       'url':
           'https://as1.ftcdn.net/v2/jpg/02/42/56/26/500_F_242562684_5VC68EHElXConqPjRazxCGuJk6btgmec.jpg',
       'id': 'ID195515435654',
       'name': 'Drone 5',
-      'data': 'Dia 20/05/2023'
+      'data': 'Dia 20/05/2023',
+      'analysis':
+          'A partir da análise de uma imagem de solo agrícola obtida por meio de um drone, verificou-se que o solo possui uma textura apropriada e indicações de nutrientes disponíveis. Não foram observadas pragas ou doenças aparentes. Recomenda-se o monitoramento regular da umidade do solo e do desenvolvimento da cobertura vegetal para garantir o sucesso do cultivo.'
     },
     {
       'url':
           'https://t4.ftcdn.net/jpg/02/44/49/75/360_F_244497513_pj9qN7NuQHYdAEMtLOEvHAjXZlv6bTbG.webp',
       'id': 'ID195515435661',
       'name': 'Drone 6',
-      'data': 'Dia 17/05/2023'
+      'data': 'Dia 17/05/2023',
+      'analysis':
+          'Após analisar uma imagem de solo agrícola capturada por um drone, foi constatado que o solo apresenta uma textura favorável e evidências de nutrientes. Não foram identificadas pragas ou doenças visíveis. Recomenda-se a monitorização da umidade do solo e o acompanhamento do crescimento da cobertura vegetal para garantir um ambiente propício ao cultivo saudável.'
     },
   ];
   List<Map<String, String>> filteredData = [];
@@ -102,8 +114,8 @@ class _HistoricoState extends State<Historico> {
                 filterSearchResults(value);
               },
               decoration: InputDecoration(
-                labelText: "Search",
-                hintText: "Search",
+                labelText: "Buscar por ID",
+                hintText: "Buscar por ID",
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(25.0)),
